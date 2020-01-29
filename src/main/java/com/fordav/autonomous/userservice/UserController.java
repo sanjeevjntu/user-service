be4d8e669d06589389612bc45e23ac15472e3510
rajanalasanjeev <rajanala.sanjeev@gmail.com>
2020-01-29 01:50:45 -0500

    Initial commit for user-service

--- src/main/java/com/fordav/autonomous/userservice/UserController.java
+++ src/main/java/com/fordav/autonomous/userservice/UserController.java
@@ -1,7 +1,6 @@
 package com.fordav.autonomous.userservice;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
