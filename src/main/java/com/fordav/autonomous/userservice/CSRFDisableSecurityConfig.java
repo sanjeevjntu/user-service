e8425312245f3ff258914d29f85c4d60f7003038
rajanalasanjeev <rajanala.sanjeev@gmail.com>
2020-02-01 14:25:48 -0500

    Initial  commit for open AOI 3 swagger ui

--- src/main/java/com/fordav/autonomous/userservice/CSRFDisableSecurityConfig.java
+++ src/main/java/com/fordav/autonomous/userservice/CSRFDisableSecurityConfig.java
@@ -1,48 +1,49 @@
-//package com.fordav.autonomous.userservice;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class CSRFDisableSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    //Step 1:: Authentication details.
-//    @Override
-//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("user")
-//                .password(passwordEncoder().encode("user"))
-//                .roles("USER")
-//                .and()
-//                .withUser("admin")
-//                .password(passwordEncoder().encode("admin"))
-//                .roles("ADMIN");
-//    }
-//
-//    // Step 2:: Below 1 is Authorization
-//    @Override
-//    protected void configure(HttpSecurity httpSecurity) throws Exception {
-//
-//        httpSecurity.csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers("/hello").hasRole("ADMIN")
-//                .antMatchers("/customers").hasAnyRole("USER", "ADMIN")
-//                .antMatchers("/**").permitAll()
-//                .and()
-//                .formLogin()
-//                .and()
-//                .httpBasic();
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//}
+package com.fordav.autonomous.userservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class CSRFDisableSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //Step 1:: Authentication details.
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN");
+    }
+
+    // Step 2:: Below 1 is Authorization
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/hello").hasRole("ADMIN")
+                .antMatchers("/customers").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/swagger-ui.html").hasRole("ADMIN")
+                .antMatchers("/**").permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/fordav/autonomous/userservice/Customer.java
+++ src/main/java/com/fordav/autonomous/userservice/Customer.java
@@ -1,5 +1,6 @@
 package com.fordav.autonomous.userservice;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -17,9 +20,16 @@ import javax.persistence.Id;
 @NoArgsConstructor
 public class Customer {
 
+    @Schema(description = "Unique identifier of the Customer.", example = "1", required = true)
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Schema(description = "Name of the Customer.", example = "Sanjeev", required = true)
+    @NotBlank(message="customer name should not be blank")
     private String name;
+
+    @Schema(description = "Vin of the Customer.", example = "VIN1234567890123", required = true)
+    @NotBlank(message="vin should not be blank")
+    //@Size(message = "vin should be 17 length", min = 17, max = 17)
     private String vin;
 }
