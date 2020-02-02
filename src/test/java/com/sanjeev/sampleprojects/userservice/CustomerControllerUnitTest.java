c3f8c3553718ea75a9690fa3c6337d5a7c200d4c
rajanalasanjeev <rajanala.sanjeev@gmail.com>
2020-02-01 20:42:04 -0500

    Uncommented test cases for security

--- src/test/java/com/sanjeev/sampleprojects/userservice/CustomerControllerUnitTest.java
+++ src/test/java/com/sanjeev/sampleprojects/userservice/CustomerControllerUnitTest.java
@@ -1,61 +1,61 @@
-//package com.sanjeev.sampleprojects.userservice;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.sanjeev.sampleprojects.userservice.domain.Customer;
-//import com.sanjeev.sampleprojects.userservice.service.CustomerService;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.ResultActions;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//
-//import java.util.UUID;
-//
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.when;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@WebMvcTest
-//@ExtendWith(SpringExtension.class)
-//public class CustomerControllerUnitTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @MockBean
-//    private CustomerService customerService;
-//
-//    @BeforeEach
-//    public void before() {
-//
-//    }
-//
-//    @Test
-//    @DisplayName("shouldSuccessfullyCreateCustomer")
-//    public void shouldSuccessfullyCreateCustomer() throws Exception {
-//        Customer mock = Customer.builder().vin("VIN1234").name("sanjeev").build();
-//        String writeValueAsString =  new ObjectMapper().writeValueAsString(mock);
-//        mock.setId(UUID.randomUUID());
-//
-//        when(customerService.create(any(Customer.class))).thenReturn(mock);
-//        ResultActions resultActions = mockMvc
-//                .perform(MockMvcRequestBuilders.post("/customers")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(writeValueAsString)
-//                );
-//        resultActions
-//                .andExpect(status().isCreated())
-//                .andExpect(jsonPath("$.name").value("sanjeev"))
-//                .andExpect(jsonPath("$.vin").value("VIN1234"))
-//                .andExpect(jsonPath("$.id").isNotEmpty());
-//
-//    }
-//}
+package com.sanjeev.sampleprojects.userservice;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sanjeev.sampleprojects.userservice.domain.Customer;
+import com.sanjeev.sampleprojects.userservice.service.CustomerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+@ExtendWith(SpringExtension.class)
+public class CustomerControllerUnitTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CustomerService customerService;
+
+    @BeforeEach
+    public void before() {
+
+    }
+
+    @Test
+    @DisplayName("shouldSuccessfullyCreateCustomer")
+    public void shouldSuccessfullyCreateCustomer() throws Exception {
+        Customer mock = Customer.builder().vin("VIN1234").name("sanjeev").build();
+        String writeValueAsString =  new ObjectMapper().writeValueAsString(mock);
+        mock.setId(UUID.randomUUID());
+
+        when(customerService.create(any(Customer.class))).thenReturn(mock);
+        ResultActions resultActions = mockMvc
+                .perform(MockMvcRequestBuilders.post("/customers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(writeValueAsString)
+                );
+        resultActions
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value("sanjeev"))
+                .andExpect(jsonPath("$.vin").value("VIN1234"))
+                .andExpect(jsonPath("$.id").isNotEmpty());
+
+    }
+}
